30f14299bb1c861f6af5ef2432bd386ca3e01d45
JulianIgnacio <montenegrojulianig@gmail.com>
2023-06-19 21:16:37 -0300

    Finalizado ejercicio 2

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int[] notas = new int[6];
+            notas[0] = 10;
+            notas[1] = 6;
+            notas[2] = 8;
+            notas[3] = 9;
+            notas[4] = 7;
+            notas[5] = 10;
+            Calcular(notas);
+            Console.WriteLine("el promedio es:{0}",Calcular(notas));
+            Console.ReadKey();
+        }
+        static int Calcular(int[] calculoNota)
+        {
+            int resultado = 0;
+            for (int i = 0; i < calculoNota.Length; i++)
+            {
+                resultado+= calculoNota[i];
+            }
+            resultado /= 6;
+            return resultado;
+        }
+    }
+}
